86084c2a3c721214fdfbc17afa3d93f753ecfbdc
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2023-04-17 15:15:18 +0200

    Find the assignment's grading standard

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
-import { router, dom } from '@artevelde-uas/canvas-lms-app';
+import { router, dom, api } from '@artevelde-uas/canvas-lms-app';
 
+
+async function getGradingStandard(courseId, assignmentId) {
+    const assignment = await api.get(`/courses/${courseId}/assignments/${assignmentId}`);
+    const gradingStandard = await api
+        .get(`/courses/${courseId}/grading_standards/${assignment.grading_standard_id}`)
+        .catch(error => null);
+
+    // The grading standard is defined within the course itself
+    if (gradingStandard !== null) {
+        return gradingStandard;
+    }
+
+    // Get the course's account ID
+    const course = await api.get(`/courses/${courseId}`);
+    let accountId = course.account_id;
+
+    // Loop over the account hierarchy until the grading standard is found
+    do {
+        const gradingStandard = await api
+            .get(`/accounts/${accountId}/grading_standards/${assignment.grading_standard_id}`)
+            .catch(error => null);
+
+        if (gradingStandard !== null) {
+            return gradingStandard;
+        }
+
+        const account = await api.get(`/accounts/${accountId}`);
+
+        accountId = account.parent_account_id;
+    } while (accountId !== null);
+
+    // Return NULL if no grading standard is found
+    return null;
+}
 
 export default function () {
-    router.onRoute('courses.gradebook.speedgrader', () => {
+    router.onRoute('courses.gradebook.speedgrader', async ({ courseId, assignmentId }) => {
+        const gradingStandard = await getGradingStandard(courseId, assignmentId);
 
     });
 
