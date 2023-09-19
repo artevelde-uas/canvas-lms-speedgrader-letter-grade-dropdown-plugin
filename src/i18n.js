30740ce7df83e1b3a8b3fe4946340f60b596d600
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2023-09-20 01:40:12 +0200

    Split translations into different files

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,14 @@
 import { addTranslations, getTranslator } from '@artevelde-uas/canvas-lms-app/services/i18n';
 
-import translations from './locales.json';
 
+// Create a context with all the [lang].json file in the 'locale' directory
+const context = require.context('../locales', false, /\.json$/);
+
+// Build the translations object
+const translations = Object.fromEntries(context.keys().map(key => [
+    key.replace(/^\.\/(.+)\.json$/, '$1'),
+    context(key)
+]));
 
 // Get the namespace from the package
 const namespace = require('../package.json').name;
