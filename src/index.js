36cce92f4bafc9953a6f0538c118c53af5e89d82
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2025-04-23 10:33:01 +0200

    Extract API call to file

--- src/index.js
+++ src/index.js
@@ -1,45 +1,11 @@
-import { router, dom, api, ui } from '@artevelde-uas/canvas-lms-app';
+import { router, dom, ui } from '@artevelde-uas/canvas-lms-app';
 
 import t from './i18n';
+import { getGradingStandard } from './api';
 import { parseLocaleNumber } from './util';
 
 import styles from './index.module.css';
 
-
-async function getGradingStandard(courseId, assignmentId) {
-    const assignment = await api.get(`/courses/${courseId}/assignments/${assignmentId}`);
-    const gradingStandard = await api
-        .get(`/courses/${courseId}/grading_standards/${assignment.grading_standard_id}`)
-        .catch(error => null);
-
-    // The grading standard is defined within the course itself
-    if (gradingStandard !== null && gradingStandard.errors === undefined) {
-        return gradingStandard;
-    }
-
-    // Get the course's account ID
-    const course = await api.get(`/courses/${courseId}`);
-    let accountId = course.account_id;
-
-    // Loop over the account hierarchy until the grading standard is found
-    do {
-        const gradingStandard = await api
-            .get(`/accounts/${accountId}/grading_standards/${assignment.grading_standard_id}`)
-            .catch(error => null);
-
-        // Grading standard found!
-        if (gradingStandard !== null && gradingStandard.errors === undefined) {
-            return gradingStandard;
-        }
-
-        const account = await api.get(`/accounts/${accountId}`);
-
-        accountId = account.parent_account_id;
-    } while (accountId !== null);
-
-    // Return NULL if no grading standard is found
-    return null;
-}
 
 export default function ({
     alwaysOpenOnFocus = false,
